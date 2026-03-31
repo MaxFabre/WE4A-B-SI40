5a3b12a3996920d71db780d7d6b5e06e635fcf83
MaxFabre <max.fabre@utbm.fr>
2026-03-31 15:34:22 +0200

    Rebonjour, après mon dernier commit, j'ai réalisé quelque chose de fou : mon accountcontroller (aussi appelé controlleur de comptes) était en réalité une final class et pas juste une class ! Vous comprendrez que je n'en revenais pas. Heuresement ceci est maintenant derrière nous.

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AccountController extends AbstractController
+class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
     public function index(): Response
