e3c3307a60ee88fc4250ed292ef736fb0925f89e
AilysGeister <samantha.guivret@gmail.com>
2026-04-15 21:50:19 +0200

    Film & Gender Model, Controllers & Views

---
+++ src/Controller/FilmController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Film;
+use App\Form\FilmType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FilmController extends AbstractController {
+    #[Route('/film', name: 'film.index', methods: ['GET'])]
+    public function index(): Response {
+        return $this->render('film/index.html.twig', [
+            'controller_name' => 'FilmController',
+        ]);
+    }
+/*
+    #[Route('/film/{slug}', name: 'film.show', methods: ['GET'])]
+    public function detail(Film $film): Response {
+        return $this->render('film/detail.html.twig', []);
+    }*/
+
+    //Route à changer en fonctions des permissions et des rôles
+    #[Route('/film/create', name: 'film.create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response {
+        $film = new Film();
+        $filmForm = $this->createForm(FilmType::class, $film);
+
+        $filmForm->handleRequest($request);
+        if ($filmForm->isSubmitted() && $filmForm->isValid()) {
+            //Enregistrement en db:
+            $entityManager->persist($film);
+            $entityManager->flush();
+
+            //Redirection avec message:
+            $this->addFlash('success', 'Le film à bien été créé.');
+            return $this->redirectToRoute('film.index');
+        }
+
+        return $this->render('film/create.html.twig', [
+            'filmForm' => $filmForm,
+        ]);
+    }
+
+    #[Route('/film/edit/{id}', name: 'film.edit', methods: ['GET', 'POST'])]
+    public function edit(Film $film, Request $request, EntityManagerInterface $entityManager): Response {
+        $filmForm = $this->createForm(FilmType::class, $film);
+        $filmForm->handleRequest($request);
+
+        if ($filmForm->isSubmitted() && $filmForm->isValid()) {
+            //Enregistrement en db:
+            $entityManager->flush();
+
+            //Redirection avec message:
+            $this->addFlash('success','Le film à bien été modifié.');
+            return $this->redirectToRoute('film.index');
+        }
+
+        return $this->render('film/edit.html.twig', [
+            'film' => $film,
+            'filmForm' => $filmForm,
+        ]);
+    }
+
+    #[Route('/film/{id}', name: 'film.delete', methods: ['DELETE'])]
+    public function delete(Film $film, EntityManagerInterface $entityManager) {
+        $entityManager->remove($film);
+        $entityManager->flush();
+        $this->addFlash('success', 'Le film à bien été supprimé.');
+        return $this->redirectToRoute('admin.carousel.index');
+    }
+}
