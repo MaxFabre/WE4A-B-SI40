e5a9fe9c9c7fae558131c4ab9f5fdf70bbcf891a
AilysGeister <samantha.guivret@gmail.com>
2026-04-19 20:33:23 +0200

    Fix auth, better form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'login')]
     public function login(AuthenticationUtils $authenticationUtils): Response {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -22,7 +22,7 @@ class SecurityController extends AbstractController {
         ]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'logout')]
     public function logout(): void {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
