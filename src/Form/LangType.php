5a260206d894aee8d5e41d971fb06f06079163de
AilysGeister <samantha.guivret@gmail.com>
2026-05-09 23:27:04 +0200

    Final

--- src/Form/LangType.php
+++ src/Form/LangType.php
@@ -2,29 +2,32 @@
 
 namespace App\Form;
 
-use App\Entity\Genre;
+use App\Entity\Lang;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class GenreType extends AbstractType {
+class LangType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
             ->add('name', TextType::class, [
                 'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Nom',
+                    'minlength' => 3,
+                    'maxlength' => 50,
+                ]
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer',
-            ])
-        ;
+                'label' => 'Valider',
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
-            'data_class' => Genre::class,
+            'data_class' => Lang::class,
         ]);
     }
 }
