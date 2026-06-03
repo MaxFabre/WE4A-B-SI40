40d283d142353c77925cdd62a6b508435ac94599
AilysGeister <samantha.guivret@gmail.com>
2026-06-03 15:22:21 +0200

    Allow api request from other app, usefull for the client at localhost:4200

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
